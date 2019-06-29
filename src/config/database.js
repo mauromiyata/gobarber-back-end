40f4295003588beb37c69db03fd949259343e007
Mauro Toshio Miyata <mauromiyata@gmail.com>
2019-06-29 14:40:50 -0300

    Criando loader de models

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'root',
+  password: '1234',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    undescoredAll: true,
+  },
+};
