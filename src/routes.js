771e823b11d54314a7b163338a9583e3b0dbd9a1
Mauro Toshio Miyata <mauromiyata@gmail.com>
2019-06-29 17:08:47 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Mauro',
-    email: 'mauromiyata@gmail.com',
-    password_hash: '1234',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
