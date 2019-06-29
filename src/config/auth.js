c5e801d4890850e1945d508e6eb3bb36edf88c50
Mauro Toshio Miyata <mauromiyata@gmail.com>
2019-06-29 20:19:08 -0300

    Autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ae482d4bf8483168b270f691258f806b',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
 
 export default routes;
